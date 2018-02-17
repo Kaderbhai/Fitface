63581daba18e6cac116ffea4abb33492c9db5657
malfernion <malfernion@gmail.com>
2018-02-17 18:36:09 +0000

    Generated drag drop component on the correct branch this time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragDropComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/drag-drop/drag-drop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drag-drop',
+  templateUrl: './drag-drop.component.html',
+  styleUrls: ['./drag-drop.component.sass']
+})
+export class DragDropComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
